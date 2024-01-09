ed89c4f749177e77d55a0ab5abe3749b0c7257bc
AlexAndrei11 <SteamAlex12@yahoo.ro>
2024-01-09 02:42:32 +0200

    Changed permissions to apis

--- user-management-microservice/src/main/java/com/iems/usermanagementmicroservice/config/SecurityConfiguration.java
+++ user-management-microservice/src/main/java/com/iems/usermanagementmicroservice/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/**").permitAll()
+                .requestMatchers("/auth/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
